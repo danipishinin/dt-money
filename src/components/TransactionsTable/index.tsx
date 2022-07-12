53c02add43c1f0aff027081b700a0f6e11c993d2
danipishinin <danipishinin>
2022-07-12 20:56:59 -0300

    inserindo transação na API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,10 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 export default function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get("transactions");
+    api.get("transactions").then((response) => setTransactions(response.data.transactions));
   }, []);
   return (
     <Container>
@@ -18,24 +29,14 @@ export default function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.0000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td>R$12.0000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="withdraw">R$12.0000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions.map((transaction) =>  (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import App from "./App";
 
 createServer({
-  routes() {
-    this.namespace = "api";
+  models: {
+    transaction: Model,
+  },
 
-    this.get("/transactions", () => {
-      return [
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-07-12 09:00:00')
         },
-      ];
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 6000,
+          createdAt: new Date('2022-07-12 10:00:00')
+        },
+      ]
+    })
+  },
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return this.schema.all('transaction')
     });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data);
+    })
   },
 });
 const root = ReactDOM.createRoot(
